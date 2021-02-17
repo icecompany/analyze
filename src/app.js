159a449a7bba41a573736c3cb916570987af8d22
asharikov <asharikov@icecompany.org>
2021-02-17 18:10:17 +0300

    New format

--- src/app.js
+++ src/app.js
@@ -8,9 +8,12 @@ window.onload = () => {
         .then((response) => {
             return response.json();
         })
-        .then((data) => {
-            addGlobalData(data);
-            addSquareTypesData(data);
+        .then((response) => {
+            let data = response.data;
+            addProjects(data);
+            addGlobalSquare(data);
+            //addGlobalData(data);
+            //addSquareTypesData(data);
         });
 }
 
@@ -47,35 +50,119 @@ let addGlobalData = (data) => {
     }
 }
 
+let addProjects = (data) => {
+    let total = document.querySelector("#summary-total");
+    for (let head of document.querySelectorAll(".projects-head")) {
+        for (let projectID in data.projects) {
+            let th = document.createElement('th');
+            th.innerText = data.projects[projectID];
+            th.colSpan = 2;
+            head.appendChild(th);
+        }
+    }
+    //Total
+    for (let projectID in data.projects) {
+        for (let what of ['square', 'money']) {
+            let th_total = document.createElement('th');
+            th_total.innerText = data.total[projectID][what];
+            total.appendChild(th_total);
+        }
+    }
+}
+
+let addGlobalSquare = (data) => {
+    let tbody = document.querySelector("#global-square-types");
+    for (let typeID in data.types) {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.innerText = data.types[typeID];
+        tr.appendChild(td);
+        for (let projectID in data.projects) {
+            for (let what of ['square', 'money']) {
+                let td = document.createElement('td');
+                try {
+                    td.innerText = data.data[typeID][projectID][what];
+                }
+                catch (e) {
+                    td.innerText = '-';
+                }
+                finally {
+                    tr.appendChild(td);
+                }
+            }
+        }
+        tbody.appendChild(tr);
+    }
+}
+
 let addSquareTypesData = (data) => {
+    addProjects(data);
     for(let i = 0; i < data.projects.length; i++) {
-        let th = document.createElement('th');
-        th.innerText = data.projects[i].title;
-        th.colSpan = 4;
-        document.querySelector("#in_pav-head").appendChild(th);
-        for(let type of [1, 2, 3, 4]) {
+        //Commercial
+        for(let type of [1, 2, 3, 4, 5, 6, 7, 8]) {
             let td_square = document.createElement('td');
             let td_money = document.createElement('td');
             let td_percent_squares = document.createElement('td');
             let td_percent_money = document.createElement('td');
-            for (let j = 0; j < data.by_square_type.length; j++) {
+            let tds = [td_square,td_percent_squares, td_money, td_percent_money];
+            for (let j = 0; j < data.by_square_type.commercial.length; j++) {
                 td_square.innerText = '0 кв. м.';
                 td_money.innerText = formatter_money.format(parseFloat(0));
                 td_percent_squares.innerText = formatter_percent.format(parseFloat(0));
                 td_percent_money.innerText = formatter_percent.format(parseFloat(0));
-                if (data.by_square_type[j].projectID === data.projects[i].id && data.by_square_type[j].square_type === type) {
-                    td_square.innerText = formatter_square.format(parseFloat(data.by_square_type[j].square)) + ' кв. м.';
-                    td_square.id = `by_square_type_data_${data.projects[i].id}_${type}`;
-                    td_money.innerText = formatter_money.format(parseFloat(data.by_square_type[j].money));
+                if (data.by_square_type.commercial[j].projectID === data.projects[i].id && data.by_square_type.commercial[j].square_type === type) {
+                    td_square.innerText = formatter_square.format(parseFloat(data.by_square_type.commercial[j].square)) + ' кв. м.';
+                    td_money.innerText = formatter_money.format(parseFloat(data.by_square_type.commercial[j].money));
                     td_percent_squares.innerText = formatter_percent.format(parseFloat(0));
                     td_percent_money.innerText = formatter_percent.format(parseFloat(0));
                     break;
                 }
             }
-            document.querySelector(`#in_pav-${type}`).appendChild(td_square);
-            document.querySelector(`#in_pav-${type}`).appendChild(td_percent_squares);
-            document.querySelector(`#in_pav-${type}`).appendChild(td_money);
-            document.querySelector(`#in_pav-${type}`).appendChild(td_percent_money);
+            for (let td of tds) document.querySelector(`#by_square_type-${type}`).appendChild(td);
+        }
+        //Sponsor
+        for (let type of ['pavilion', 'street']) {
+            let td_square = document.createElement('td');
+            let td_money = document.createElement('td');
+            let td_percent_squares = document.createElement('td');
+            let td_percent_money = document.createElement('td');
+            let tds = [td_square,td_percent_squares, td_money, td_percent_money];
+            for (let j = 0; j < data.by_square_type.sponsor.length; j++) {
+                td_square.innerText = '0 кв. м.';
+                td_money.innerText = formatter_money.format(parseFloat(0));
+                td_percent_squares.innerText = formatter_percent.format(parseFloat(0));
+                td_percent_money.innerText = formatter_percent.format(parseFloat(0));
+                if (data.by_square_type.sponsor[j].projectID === data.projects[i].id && data.by_square_type.sponsor[j].tip === type) {
+                    td_square.innerText = formatter_square.format(parseFloat(data.by_square_type.sponsor[j].square)) + ' кв. м.';
+                    td_money.innerText = formatter_money.format(parseFloat(data.by_square_type.sponsor[j].money));
+                    td_percent_squares.innerText = formatter_percent.format(parseFloat(0));
+                    td_percent_money.innerText = formatter_percent.format(parseFloat(0));
+                    break;
+                }
+            }
+            for (let td of tds) document.querySelector(`#by_square_${type}-sponsor`).appendChild(td);
+        }
+        //Non-commercial
+        for (let type of ['pavilion', 'street']) {
+            let td_square = document.createElement('td');
+            let td_money = document.createElement('td');
+            let td_percent_squares = document.createElement('td');
+            let td_percent_money = document.createElement('td');
+            let tds = [td_square,td_percent_squares, td_money, td_percent_money];
+            for (let j = 0; j < data.by_square_type.noc.length; j++) {
+                td_square.innerText = '0 кв. м.';
+                td_money.innerText = formatter_money.format(parseFloat(0));
+                td_percent_squares.innerText = formatter_percent.format(parseFloat(0));
+                td_percent_money.innerText = formatter_percent.format(parseFloat(0));
+                if (data.by_square_type.noc[j].projectID === data.projects[i].id && data.by_square_type.noc[j].tip === type) {
+                    td_square.innerText = formatter_square.format(parseFloat(data.by_square_type.noc[j].square)) + ' кв. м.';
+                    td_money.innerText = formatter_money.format(parseFloat(data.by_square_type.noc[j].money));
+                    td_percent_squares.innerText = formatter_percent.format(parseFloat(0));
+                    td_percent_money.innerText = formatter_percent.format(parseFloat(0));
+                    break;
+                }
+            }
+            for (let td of tds) document.querySelector(`#by_square_${type}-non-commercial`).appendChild(td);
         }
     }
 }
