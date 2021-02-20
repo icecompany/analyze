16cb1ac1bdbb188956debe52eb78c2ba64da2098
asharikov <asharikov@icecompany.org>
2021-02-20 15:44:14 +0300

    URLs

--- src/app.js
+++ src/app.js
@@ -2,8 +2,37 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let all_projects = document.querySelector("#all-projects");
+let projects = document.querySelectorAll(".select-project input[type='checkbox']");
+
 window.onload = () => {
-    let url = "data.json";
+    all_projects.style.display = 'none';
+    document.querySelector("#select-family").addEventListener('change', loadData, false);
+    for (let project of projects) project.addEventListener('change', loadData, false);
+}
+
+let loadMore = (square_type, commercial) => {
+    let familyID = document.querySelector("#select-family").value;
+    if (isNaN(parseInt(familyID))) return;
+    let url = getURITypes(familyID, square_type, commercial);
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let data = response.data;
+            addMore(data, "#more-companies");
+            addTotal(data, "#more-total");
+        });
+}
+
+let loadData = () => {
+    let familyID = document.querySelector("#select-family").value;
+    if (isNaN(parseInt(familyID))) return;
+    all_projects.style.display = 'block';
+    for (let project of document.querySelectorAll(".select-project")) project.style.display = (parseInt(project.dataset.id) !== parseInt(familyID)) ? 'none' : 'block';
+    document.querySelector("#project-title").textContent = document.querySelector(`#select-family option[value='${familyID}']`).textContent;
+    let url = getURISummary(familyID);
     fetch(url)
         .then((response) => {
             return response.json();
@@ -19,21 +48,34 @@ window.onload = () => {
             addSquare(data.squares, "#street-types", 'street');
             addTotal(data.squares, "#street-total", 'street');
         });
+}
 
-    let url_more = "more.json";
-    fetch(url_more)
-        .then((response) => {
-            return response.json();
-        })
-        .then((response) => {
-            let data = response.data;
-            addMore(data, "#more-companies");
-            addTotal(data, "#more-total");
-        });
+let getURISummary = (familyID) => {
+    let url = `/administrator/index.php?option=com_janalyze&task=summary.execute&familyID=${familyID}&format=json`;
+    let excludeID = getExcludedProjects(familyID);
+    if (excludeID.length > 0) url += excludeID;
+    return url;
+}
+
+let getURITypes = (familyID, square_type, commercial) => {
+    let url = `/administrator/index.php?option=com_janalyze&task=types.execute&familyID=${familyID}&square_type=${square_type}&commercial=${commercial}&format=json`;
+    let excludeID = getExcludedProjects(familyID);
+    if (excludeID.length > 0) url += excludeID;
+    return url;
+}
+
+let getExcludedProjects = (familyID) => {
+    let exclude = '';
+    for (let project of projects) {
+        if (!project.checked && parseInt(familyID) === parseInt(project.dataset.family)) exclude += `&excludeID[]=${project.dataset.id}`;
+    }
+    return exclude;
 }
 
 let addProjects = (data) => {
-    for (let head of document.querySelectorAll(".projects-head")) {
+    let thead = document.querySelectorAll(".projects-head");
+    for (let head of thead) {
+        head.innerHTML = '<th>Площадь</th>';
         for (let projectID in data.projects) {
             let th = document.createElement('th');
             th.innerText = data.projects[projectID];
@@ -45,6 +87,7 @@ let addProjects = (data) => {
 
 let addTotal = (data, selector, square_type = false) => {
     let total = document.querySelector(selector);
+    total.innerHTML = '<th>Итого</th>';
     for (let projectID in data.projects) {
         for (let what of ['square', 'money']) {
             let th_total = document.createElement('th');
@@ -56,10 +99,18 @@ let addTotal = (data, selector, square_type = false) => {
 
 let addSquare = (data, selector, square_type = false) => {
     let tbody = document.querySelector(selector);
+    tbody.innerHTML = '';
     for (let typeID in (!square_type) ? data.types: data.types[square_type]) {
         let tr = document.createElement('tr');
         let td = document.createElement('td');
-        td.innerText = (!square_type) ? data.types[typeID] : data.types[square_type][typeID];
+        let a = document.createElement('a');
+        a.href = '#';
+        a.addEventListener('click', () => {
+            loadMore(typeID, 'commercial');
+            return false;
+        });
+        a.text = (!square_type) ? data.types[typeID] : data.types[square_type][typeID];
+        td.appendChild(a);
         tr.appendChild(td);
         for (let projectID in data.projects) {
             for (let what of ['square', 'money']) {
@@ -79,6 +130,7 @@ let addSquare = (data, selector, square_type = false) => {
 
 let addMore = (data, selector) => {
     let tbody = document.querySelector(selector);
+    tbody.innerHTML = '';
     for (let company in data.companies) {
         let tr = document.createElement('tr');
         let td = document.createElement('td');
